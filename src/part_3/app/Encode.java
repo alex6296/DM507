13c589b9c5372649a2ae8cf9f1e6349c135cff89
Alex <32260519+alex6296@users.noreply.github.com>
2019-05-14 19:24:54 +0200

    code syntax bitch

--- src/part_3/app/Encode.java
+++ src/part_3/app/Encode.java
@@ -119,7 +119,7 @@ public class Encode {
         int n = C.getHeapSize();
         PQ Q = C;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n-1; i++) {
 
             System.out.println("-- loop " + i + "--");
             System.out.println("heapsize = " + C.getHeapSize());
